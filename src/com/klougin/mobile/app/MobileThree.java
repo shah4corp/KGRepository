ef7914f20816aa8c7c2280f84e1b67f81395f364
Mohammed Shahnawaz <shahnawazm@surfbi.local>
2019-06-24 17:48:59 +0530

    pushed // 6/24/2019

--- src/com/klougin/mobile/app/MobileThree.java
+++ src/com/klougin/mobile/app/MobileThree.java
@@ -14,7 +14,7 @@ public class MobileThree extends BaseEngine{
 		getAppium().findElement(By.xpath("//*[contains(@text,'ALLOW')]")).click();
 		getAppium().findElement(By.xpath("//*[contains(@text,'ALLOW')]")).click();
 		getAppium().findElement(By.xpath("//*[contains(@text,'ALLOW')]")).click();
-		getAppium().findElement(By.id("com.es.kloudgin:id/chooseProfile")).click();
+		//getAppium().findElement(By.id("com.es.kloudgin:id/chooseProfile")).click();
 		/*getAppium().findElement(By.xpath("//*[contains(@text,'UAT')]")).click();
 		getAppium().findElement(By.xpath("//*[contains(@text,'Usernamae')]")).sendKeys("treno");
 		getAppium().findElement(By.xpath("//*[contains(@text,'Cloud Instance')]")).sendKeys("ht-sc");
