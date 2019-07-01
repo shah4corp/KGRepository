af2206a04cf8da1fe941342110198db8c56f5118
Mohammed Shahnawaz <shahnawazm@surfbi.local>
2019-07-01 15:39:45 +0530

    7/1/2019 commit

--- src/com/klougin/mobile/app/WriteDataExcelDemo.java
+++ src/com/klougin/mobile/app/WriteDataExcelDemo.java
@@ -2,7 +2,9 @@ package com.klougin.mobile.app;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.sl.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -10,16 +12,15 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class WriteDataExcelDemo {
 	static FileInputStream fip;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws EncryptedDocumentException, IOException {
 		try {
-			fip; = new FileInputStream(System.getProperty("user.dir") + "\\Excel_sheets\\Leave Balances.xlsx");
+			fip = new FileInputStream(System.getProperty("user.dir") + "\\Excel_sheets\\Leave Balances.xlsx");
 		} catch (FileNotFoundException e) {
             System.out.println("check the dynamic path of file Please enter the valid path");
 			e.printStackTrace();
 		}
 	Workbook workbook = WorkbookFactory.create(fip);
 	Sheet sheet = (Sheet) workbook.getSheet("myData");
-	Row row0 = 
 	
 	
 	}
