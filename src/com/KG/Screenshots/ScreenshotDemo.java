17ae7cdc60039b2366e0f48ac5ef223d6f898bc1
Mohammed Shahnawaz <shahnawazm@surfbi.local>
2019-07-09 21:55:46 +0530

    7/9/2019 commit

--- src/com/KG/Screenshots/ScreenshotDemo.java
+++ src/com/KG/Screenshots/ScreenshotDemo.java
@@ -1,4 +1,4 @@
-package com.klougin.mobile.app;
+package com.KG.Screenshots;
 
 import java.io.File;  
 import java.io.IOException;
--- src/com/KG/Screenshots/ScreenshotOne.java
+++ src/com/KG/Screenshots/ScreenshotOne.java
@@ -1,4 +1,4 @@
-package com.klougin.mobile.app;
+package com.KG.Screenshots;
 
 import java.io.File;
 import java.lang.reflect.Method;
@@ -24,13 +24,20 @@ import org.testng.annotations.Test;
 
 public class ScreenshotOne {
 	WebDriver d;
+	public void screenshot(String imagename) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot)d;
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("D:\\"+ imagename + ".png"));
+	}
 	@Test
-	public void login() {
+	public void logIn() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\shahnawazm\\workspace\\MobileTest\\Drivers\\chromedriver.exe");
 	    d = new ChromeDriver();
-		d.manage().timeouts().implicitlyWait(60 , TimeUnit.SECONDS);
+		d.manage().timeouts().implicitlyWait(20 , TimeUnit.SECONDS);
 		d.get("https://www.gmail.com");
+		d.findElement(By.id("identifierId")).sendKeys("shah4corp@gmail.com");
 		d.findElement(By.id("identifierI")).sendKeys("shah4corp@gmail.com");
+
 		
 	}
 	@BeforeMethod
@@ -40,9 +47,7 @@ public class ScreenshotOne {
 	@AfterMethod
 	public void afterMethod(ITestResult result) throws IOException {
 		if(ITestResult.FAILURE==result.getStatus()) {
-			TakesScreenshot ts = (TakesScreenshot)d;
-	        File file = ts.getScreenshotAs(OutputType.FILE);
-	        FileUtils.copyFile(file, new File("D:\\gmailHomepage.png"));	
+			screenshot(result.getName());	
 		}
 	}
 
