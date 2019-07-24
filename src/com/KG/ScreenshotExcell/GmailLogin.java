91d62ebc64383d221275bb2a194a7187a28e73d8
Mohammed Shahnawaz <shahnawazm@surfbi.local>
2019-07-24 10:53:17 +0530

    7/24/2019 commit

--- src/com/KG/ScreenshotExcell/GmailLogin.java
+++ src/com/KG/ScreenshotExcell/GmailLogin.java
@@ -8,7 +8,7 @@ import com.KG.utilities.DriverUtility;
 public class GmailLogin {
 	@Test
 	public void LogIn() {
-		DriverUtility.openBrowser("chrome");
+		DriverUtility.openBrowser("ie");
 	    DriverUtility.getDriver().get("https://www.gmail.com");
 	    DriverUtility.getDriver().findElement(By.id("identifierId")).sendKeys("shah4corp@gmail.com");
 	    DriverUtility.getDriver().findElement(By.id("dentifierId")).sendKeys("shah4corp@gmail.com");
